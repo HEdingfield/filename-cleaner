d2ee0139c5d82853148cf243a67c1b32e2320b35
HEdingfield <hedingfield@geosyntec.com>
2017-05-16 17:16:56 -0700

    Tests: moved file and directory creation to setUp; fixed tearDown to recursively delete files and directories.

--- test_fc.py
+++ test_fc.py
@@ -5,36 +5,47 @@ from fc import filename_cleaner
 
 class Test(unittest.TestCase):
     def setUp(self):
-        # Create temporary files and directories to test on
-        self.input_path = Path.cwd().__str__() + "\\testing"
+        self.input_path = Path.cwd().__str__() + '\\testing'
         if not Path(self.input_path).exists():
             Path(self.input_path).mkdir(parents=True)
         self.bad_chars = '\/*?:"<>|'
-        self.replacement_char = "_"
-        self.clean_type = "both"
+        self.replacement_char = '_'
+        self.clean_type = 'both'
         self.actually_rename = False
+
+        # Create temporary files and directories to test on
+        Path(self.input_path + "\\test1.txt").touch()
+        Path(self.input_path + '\\t1est.t1xt').touch()
+        Path(self.input_path + '\\test.txt').touch()
+        bad_dir_path = self.input_path + '\\bad1dir'
+        Path(bad_dir_path).mkdir(parents=True)
+        Path(bad_dir_path + "\\test1.txt").touch()
+        Path(bad_dir_path + '\\t1est.t1xt').touch()
+        Path(bad_dir_path + '\\test.txt').touch()
+        good_dir_path = self.input_path + '\\gooddir'
+        Path(good_dir_path).mkdir(parents=True)
+        Path(good_dir_path + "\\test1.txt").touch()
+        Path(good_dir_path + '\\t1est.t1xt').touch()
+        Path(good_dir_path + '\\test.txt').touch()
 
     def tearDown(self):
         # Delete all temporary files and directories created
-        p = Path(self.input_path)
-        for f in p.glob('**/*'):
-            if f.is_file():
-                f.unlink()
-        p.rmdir()
+        def delete_folder(path):
+            for sub in path.iterdir():
+                if sub.is_dir():
+                    delete_folder(sub)
+                else:
+                    sub.unlink()
+            path.rmdir()
+
+        delete_folder(Path(self.input_path))
 
     def test_bad_input_dir(self):
-        input_path = self.input_path + "\\bad_path\\"
+        input_path = self.input_path + '\\bad_path\\'
         self.assertRaises(NotADirectoryError, filename_cleaner, input_path, self.bad_chars, self.replacement_char,
                           self.clean_type, self.actually_rename)
 
     def test_no_rename(self):
-        # Create files
-        Path(self.input_path + '\\test1.txt').touch()
-        Path(self.input_path + '\\t1est.t1xt').touch()
-        Path(self.input_path + '\\test.txt').touch()
-        # TODO: create files in subdirectories, some of which are renamed
-        # TODO: create directories
-
         names_set_before = set([str(name) for name in Path(self.input_path).iterdir()])
         filename_cleaner(self.input_path, self.bad_chars, self.replacement_char, self.clean_type, self.actually_rename)
         names_set_after = set([str(name) for name in Path(self.input_path).iterdir()])
